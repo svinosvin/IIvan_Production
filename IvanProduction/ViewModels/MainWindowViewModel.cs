9b708a7b70ab121a8d5a2e43294c0212540c8295
svinosvin <78436343+svinosvin@users.noreply.github.com>
2022-04-04 00:04:20 +0300

    Подключение MainWindow к ViewModel

--- IvanProduction/ViewModels/MainWindowViewModel.cs
+++ IvanProduction/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IvanProduction.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace IvanProduction.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+
     }
 }
