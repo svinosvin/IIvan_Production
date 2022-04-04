68b93d4eb0086ae2c54adc0147ed2fe78dd6a9a0
svinosvin <78436343+svinosvin@users.noreply.github.com>
2022-04-04 13:29:37 +0300

    Отредактировал View Авторизация

--- IvanProduction/ViewModels/MainWindowViewModel.cs
+++ IvanProduction/ViewModels/MainWindowViewModel.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace IvanProduction.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+
+        
 
     }
 }
--- IvanProduction/Views/Windows/MainWindow.xaml.cs
+++ IvanProduction/Views/Windows/MainWindow.xaml.cs
@@ -20,10 +20,44 @@ namespace IvanProduction
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string login = "Username";
+        private const string password = "Password";
         public MainWindow()
         {
             InitializeComponent();
-            
+           
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(LoginTB.Text == login)
+            LoginTB.Text = "";           
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(LoginTB.Text))
+            {
+                LoginTB.Text = login;
+            }
+        }
+
+        private void PasswordTB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTB.Password == password)
+                PasswordTB.Password = "";
+        }
+
+        private void PasswordTB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(PasswordTB.Password))
+            {
+                PasswordTB.Password = password;
+            }
         }
     }
 }
